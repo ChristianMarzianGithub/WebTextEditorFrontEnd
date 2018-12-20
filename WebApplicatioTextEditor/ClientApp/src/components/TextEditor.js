1ccce12a034a488c945ee835719723780db9d4eb
Marzian <marzian@innosoft.local>
2018-12-20 16:53:48 +0100

    compomend build without webservice-connect

--- WebApplicatioTextEditor/ClientApp/src/components/TextEditor.js
+++ WebApplicatioTextEditor/ClientApp/src/components/TextEditor.js
@@ -1,15 +1,38 @@
 ﻿import React, { Component } from 'react';
+import './TextEditor.css';
 
 export class TextEditor extends Component {
     displayName = TextEditor.name
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            valueTextMain: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    handleChange(event) {
+        this.setState({ valueTextMain: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.valueTextMain);
+        event.preventDefault();//prevents reload
+    }
+
 
     render() {
-
         return (
-                <div>
-                    <p>test</p>
-                </div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <textarea id='textAreaMain' value={this.state.valueTextMain} onChange={this.handleChange} />
+                    <input type="submit" value="senden" />
+                </form>
+            </div>
             );
     }
+
+
 }
