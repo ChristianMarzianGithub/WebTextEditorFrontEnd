54aeed51f7004f1423a3e96774e742a2848f31e1
Marzian <marzian@innosoft.local>
2018-12-20 16:07:12 +0100

    initial commit

---
+++ WebApplicatioTextEditor/ClientApp/src/components/TextEditor.js
@@ -0,0 +1,15 @@
+﻿import React, { Component } from 'react';
+
+export class TextEditor extends Component {
+    displayName = TextEditor.name
+
+
+    render() {
+
+        return (
+                <div>
+                    <p>test</p>
+                </div>
+            );
+    }
+}
